347f6556ca6203051966be1886ecaf6839a7e2f7
UnbroKen <unbroken2650@gmail.com>
2022-02-27 14:29:06 +0000

    eslint-airbnb 세팅

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-filename-extension */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-    <App />
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("react-container"));
