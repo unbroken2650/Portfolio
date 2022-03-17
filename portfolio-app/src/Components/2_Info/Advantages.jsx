ef748e9df7bff2adf99f124cb4426e992cf1f8c5
UnbroKen <unbroken2650@gmail.com>
2022-03-17 14:04:37 +0000

    info 페이지 기본 구조 완성

--- portfolio-app/src/Components/2_Info/Advantages.jsx
+++ portfolio-app/src/Components/2_Info/Advantages.jsx
@@ -8,23 +8,28 @@ function Advantages() {
      src="https://www.duemarichapter.it/wp-content/uploads/2018/11/blog-ph-1.jpg"
      alt="sample"
     ></img>
-    <p>맡은 일에 최선을 다하는 책임감</p>
+    <p>
+     맡은 일에 <br></br>최선을 다하는 책임감
+    </p>
    </div>
    <div className="imgright">
     <img
      src="https://www.duemarichapter.it/wp-content/uploads/2018/11/blog-ph-1.jpg"
      alt="sample"
     ></img>
-    <p>타인의 의견을 수렴하고 존중하는 자세</p>
+    <p>
+     타인의 의견을 수렴하고 <br></br>존중하는 자세
+    </p>
    </div>
    <div className="imgleft">
     <img
      src="https://www.duemarichapter.it/wp-content/uploads/2018/11/blog-ph-1.jpg"
      alt="sample"
     ></img>
+    <p>
+     한 번 시작하면 <br></br>끝까지 몰입하는 집중력
+    </p>
    </div>
-
-   <p>한 번 시작하면 끝까지 몰입하는 집중력</p>
   </section>
  );
 }
