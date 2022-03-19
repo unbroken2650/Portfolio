b25121978c1fb54150aeeb00f4b025ab94398093
UnbroKen <unbroken2650@gmail.com>
2022-03-19 14:36:41 +0000

    Info 레이아웃 수정

--- portfolio-app/src/Components/Main.jsx
+++ portfolio-app/src/Components/Main.jsx
@@ -1,14 +1,16 @@
-import { React } from 'react';
-import Landing from './Landing';
-import Info from './Info';
+import { React } from "react";
+import Landing from "./Landing";
+import Info from "./Info";
+import Projects from "./Projects";
 
 function Main() {
-  return (
-    <main>
-      <Landing />
-      <Info />
-    </main>
-  );
+ return (
+  <main>
+   <Landing />
+   <Info />
+   <Projects />
+  </main>
+ );
 }
 
 export default Main;
