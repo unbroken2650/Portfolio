f47c208faf701664898f551235a61b5671853072
UnbroKen <unbroken2650@gmail.com>
2022-04-11 15:06:32 +0000

    Info 페이지 수정

--- portfolio-app/src/Components/2_Info/Advantages.jsx
+++ portfolio-app/src/Components/2_Info/Advantages.jsx
@@ -1,34 +1,37 @@
 import React from "react";
-
+import { ThumbsUpIcon } from "../../icons";
 function Advantages() {
-  return (
-    <section id="advantages">
-      <div className="imgleft">
-        <img
-          src="https://www.duemarichapter.it/wp-content/uploads/2018/11/blog-ph-1.jpg"
-          alt="sample"></img>
-        <p>
-          맡은 일에 <br></br>최선을 다하는 책임감
-        </p>
-      </div>
-      <div className="imgright">
-        <img
-          src="https://www.duemarichapter.it/wp-content/uploads/2018/11/blog-ph-1.jpg"
-          alt="sample"></img>
-        <p>
-          타인의 의견을 수렴하고 <br></br>존중하는 자세
-        </p>
-      </div>
-      <div className="imgleft">
-        <img
-          src="https://www.duemarichapter.it/wp-content/uploads/2018/11/blog-ph-1.jpg"
-          alt="sample"></img>
-        <p>
-          한 번 시작하면 <br></br>끝까지 몰입하는 집중력
-        </p>
-      </div>
-    </section>
-  );
+ const advantages = [
+  {
+   name: "책임감",
+   desc: "주어진 업무에 최선을 다하는 성격을 가지고 있습니다.",
+  },
+  {
+   name: "포용력",
+   desc: "타인의 의견을 수렴하고 존중하는 자세를 갖추고 있습니다.",
+  },
+  { name: "집중력", desc: "한번 시작하면 끝까지 몰입합니다." },
+ ];
+ return (
+  <section className="info-detail" id="Advantages">
+   <img
+    src="https://www.duemarichapter.it/wp-content/uploads/2018/11/blog-ph-1.jpg"
+    alt="sample"
+   ></img>
+   <article id="info-advantages">
+    <h3>Advantages</h3>
+    {advantages.map((a, index) => (
+     <div className="advantages-adv">
+      <h4>{a.name}</h4>
+      <p>
+       <ThumbsUpIcon />
+       {a.desc}
+      </p>
+     </div>
+    ))}
+   </article>
+  </section>
+ );
 }
 
 export default Advantages;
